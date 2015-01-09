c7dd0376d21b3bcb335dc5b8689904bb6ccf8f2e
Tsur <prucheta@gmail.com>
2015-01-09 17:25:31 +0100

    #FEAT basic examples added

---
+++ examples/basic_search.js
@@ -0,0 +1,45 @@
+var Rae = require('../rae');
+
+var outputResults = function(lema) {
+
+  console.log("Etimologia:", lema.etymology);
+
+  lema.definitions.forEach(function(definition) {
+
+    console.log(definition);
+
+  });
+
+};
+
+// Callback
+Rae.search("repositorio", function(err, lemas) {
+
+  if (err) {
+
+    return console.error(err);
+
+  }
+
+  console.log('Callback Example:');
+
+  lemas.forEach(outputResults);
+
+});
+
+// Promises
+var promise = Rae.search("repositorio");
+
+promise
+  .then(function(lemas) {
+
+    console.log('Promise Example:');
+
+    lemas.forEach(outputResults);
+
+  })
+  .fail(function(err) {
+
+    console.error(err);
+
+  });
