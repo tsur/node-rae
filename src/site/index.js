e2f4674ec80917785e1de823bba3c254d06c127f
Tsur <prucheta@gmail.com>
2017-11-08 20:13:00 +0100

    feat(site): Copie content to clipboard

--- src/site/index.js
+++ src/site/index.js
@@ -5,14 +5,27 @@ import Clipboard from 'clipboard';
 
 const ENTER_KEY = 13;
 
+function getParameterByName(paramName, url = window.location.href) {
+  const regex = new RegExp(`[?&]${paramName.replace(/[[\]]/g, '\\$&')}(=([^&#]*)|&|#|$)`);
+  const results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
 async function getContent(word, action) {
   const loaderElement = document.querySelector('.loader');
   const matchesElement = document.querySelector('.matches');
   const matchesContentElement = document.querySelector('.definitions');
+  const urlElement = document.querySelector('.url');
   try {
     matchesContentElement.remove();
     loaderElement.classList.remove('hidden');
     matchesElement.classList.add('hidden');
+    urlElement.classList.add('hidden');
+    urlElement.textContent = `http://zuri.website/rae?${action === 'search'
+      ? 'buscar'
+      : 'id'}=${encodeURI(word)}`;
     const serverResponse = await fetch(`${URL}${action}/${encodeURI(word)}`, {
       method: 'GET',
     });
@@ -43,11 +56,13 @@ async function getContent(word, action) {
     loaderElement.classList.add('hidden');
   }
 }
-
 function init() {
   const logoElement = document.querySelector('.logo');
   const searchInputElement = document.querySelector('.search-bar');
   const matchesElement = document.querySelector('.matches');
+  const urlElement = document.querySelector('.url');
+  const shareElement = document.querySelector('.share');
+
   // Set image src
   logoElement.src = raeLogo;
   // Set input handler
@@ -68,6 +83,15 @@ function init() {
   clipboard.on('success', () => {
     setTimeout(() => searchInputElement.focus(), 1000);
   });
+  shareElement.addEventListener('click', () => {
+    urlElement.classList.remove('hidden');
+  });
+  if (getParameterByName('buscar')) {
+    searchInputElement.value = getParameterByName('buscar');
+    setTimeout(() => getContent(searchInputElement.value, 'search'), 500);
+  } else if (getParameterByName('id')) {
+    setTimeout(() => getContent(getParameterByName('id'), 'fetch'), 500);
+  }
 }
 
 window.onload = init;
